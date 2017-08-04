706e8e4bdff7b70693c48f1543c8a11365de2a48
Léo Câmara <leo.camara@gmail.com>
2017-08-03 23:09:05 -0300

    problema instituicao

--- app.js
+++ app.js
@@ -22,14 +22,13 @@ app.get("/", function(req, res) {
 app.get("/result", function(req, res) {
     var search = req.query.search;
     var type = req.query.dataType;
-    console.log(type);
     Data.find({
         [type]: search
     }, function(err, results) {
         if (err) {
             console.log(err);
         } else {
-            res.render("result", { data: { results: results, search: search, [type]: type } });
+            res.render("result", { data: { results: results, search: search, type: req.query.type } });
         }
     });
 });
